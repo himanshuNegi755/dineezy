21f4e9e8aa60e5165a355f9fbabe4e8dad74c29f
Rajeeb Nayak <rajeebnayak37@gmail.com>
2020-07-31 23:08:28 +0530

    Worked on Kitchen table page frontend

--- client/src/components/kitchen/shopPage.js
+++ client/src/components/kitchen/shopPage.js
@@ -7,10 +7,10 @@ import Footer from '../footer';
 
 
 const ShopPage = (props) => {
-    
+
     const [noOfTables, setNoOfTables] = useState(1);
     const [currentTable, setCurrentTable] = useState(1);
-    
+
     useEffect(() => {
         axios.get(`${process.env.REACT_APP_BACKEND_API}/tables_no?userEmail=${props.match.params.email}&shopId=${props.match.params.shopId}`)
         .then(res => {
@@ -18,42 +18,39 @@ const ShopPage = (props) => {
             setNoOfTables(res.data.noOfTables);
         })
     }, [])
-    
+
     const loadTablesFunction = (noOfTables) => {
         let tableArr = []
         for (var i=1; i<=noOfTables; i++) {
             tableArr[i] = i;
         }
-        
+
         const list = tableArr.map((tableNo) =>
             <div key={tableNo}>
-
-                <li className="email-ind" onClick={ () => {setCurrentTable(tableNo)}}>
-                    <span className="accesser-email">{tableNo}</span>
+                <li className="tableNo-ind" onClick={ () => {setCurrentTable(tableNo)}}>
+                    {tableNo}
                 </li>
-
             </div>
             );
             return (list);
-        
+
     }
-    
+
     return (
         <div className="parent-div">
             <NavbarForSite />
             <div className="main-container row">
                 <div className="col-lg-3 col-md-4 tableNo-col">
                     <div className="tableNo-heading">Tables Numbers</div>
-                        {loadTablesFunction(noOfTables)}
                     <div className="tableNo-list">
-
+                        {loadTablesFunction(noOfTables)}
                     </div>
                 </div>
                 <div className="col-lg-9 col-md-8 menu-col">
 
                     <div className="tableNo-name row">
-                        <div className="col-lg-9 col-sm-8">
-                            Table No {currentTable}
+                        <div className="col-lg-9 col-md-8 col-sm-8 tableNo-text">
+                            <i class="far fa-clipboard"></i> Table No {currentTable}.
                         </div>
                         <div className="col-lg-3 col-sm-4">
                             <button className="add-new-item-btn">
