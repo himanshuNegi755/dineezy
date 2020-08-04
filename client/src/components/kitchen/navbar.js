57e84f7b4ec764316d318a34af45790f881f5992
Rajeeb Nayak <rajeebnayak37@gmail.com>
2020-08-04 17:58:30 +0530

    Changed the logo

--- client/src/components/kitchen/navbar.js
+++ client/src/components/kitchen/navbar.js
@@ -2,28 +2,27 @@ import React from 'react';
 import './navbar.css';
 //import GoogleButton from '../googleButton';
 //import LogoutButton from '../logoutButton';
-import DMLogo from '../../images/dmLogo.svg';
+import DMLogo from '../../images/dineezyLogo.svg';
 
 const NavbarForSite = (props) => {
 
     return (
-        <nav className="navbar navbar-expand-lg navbar-dark">
-            <a id="navbar-brand" href="/">
+        <nav className="navbar navbar-expand-lg navbar-dark row">
+            <div className="col-3"><a id="navbar-brand" href="/">
                 <span>
                     <img id="brand-logo" src={DMLogo} alt="Company Logo"/>
                 </span>
-            </a>
-            <span className="restaurant-name">{props.shopName}<i className="fas fa-cauldron"></i></span>
+            </a></div>
+            <div className="restaurant-name col-6">{props.shopName} Kitchen</div>
 
-            <ul className="navbar-nav ml-auto">
+            <div className="col-3"><ul className="navbar-nav">
                 <li className="nav-item">
                     <a className="nav-link" href="#"><strong>Table Orders</strong></a>
                 </li>
                 <li className="nav-item">
                     <a className="nav-link" href="#footer">Help ?</a>
                 </li>
-            </ul>
-
+            </ul></div>
         </nav>
 
         );
